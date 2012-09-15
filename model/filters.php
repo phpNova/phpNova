830151797884b869d2ab9e5a54b3f3c8c06680a7
Kris Craig <Kris.Craig@gmail.com>
2012-09-15 04:42:41 -0700

    The model.  Still needs module checking integration before any of it will work.

---
+++ model/filters.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Place your custom filters here.
+ * 
+ * The first argument must ALWAYS be reserved for the input HTML string!
+ * This means that the second argument in the function will correspond to the 
+ * first argument that the user specifies in the JavaScript.
+ * 
+ * Your function is expected to return a string.  That output will then be passed 
+ * either to the web browser or the next function in the user-specified sequence.
+ * 
+ * If your function has arguments (in addition to input), I would recommend you 
+ * make them optional for maximum permissiveness.
+ * 
+ * --Kris
+ */
+class filters
+{
+	
+}
